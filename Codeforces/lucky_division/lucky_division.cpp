a786772c60b89166f7550d2ea0efe8aa1775eb3a
Maverick100199 <1siddharthkulkarni007@gmail.com>
2019-08-04 19:41:59 +0530

    tweak

--- Codeforces/lucky_division/lucky_division.cpp
+++ Codeforces/lucky_division/lucky_division.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main() {
-	int n, arr[] = {4, 7, 44, 47, 74, 77, 444, 447, 474, 477, 744, 747, 774, 777};
+	int n, arr[] = {4, 7, 47, 74, 447, 474, 477, 744, 747, 774};
 	cin >> n;
-	for(int i = 0; i < 14; i++) {
+	for(int i = 0; i < 10; i++) {
 		if(n % arr[i] == 0) goto label;
 	}
 	cout << "NO\n";
